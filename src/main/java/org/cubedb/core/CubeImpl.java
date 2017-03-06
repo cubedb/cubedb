ab63603047b3f3383818f035989a8ef751905011
Demeter Sztanko <demeter.sztanko@corp.badoo.com>
2017-03-06 16:25:35 +0000

    [master]: Typo in log

--- src/main/java/org/cubedb/core/CubeImpl.java
+++ src/main/java/org/cubedb/core/CubeImpl.java
@@ -118,7 +118,7 @@ public class CubeImpl implements Cube {
 		Map<String, List<DataRow>> groupedData = data.stream().collect(Collectors.groupingBy(DataRow::getPartition));
 		// insertSequential(groupedData);
 		//log.debug("The following partitions detected: {}", groupedData.keySet());
-		log.info("Size of items: {}, number of groups", data.size(), groupedData.size());
+		log.info("Size of items: {}, number of groups: {}", data.size(), groupedData.size());
 		if (data.size() < 1500 || groupedData.size() <= 3)
 		{
 			log.info("Using sequential insert");
