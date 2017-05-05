20279d2fad1114d73faf0b0bb295e25ae5954c45
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-05-05 15:50:57 +0100

    Drop commented code

--- src/main/java/org/cubedb/offheap/CounterContainer.java
+++ src/main/java/org/cubedb/offheap/CounterContainer.java
@@ -42,7 +42,6 @@ public class CounterContainer {
 	}
 
 	private Map<MultiKey, Long> sideCounters;
-    // private long[][][][] sideCounters;
 
 	private Lookup fieldLookup;
 	private Lookup metricLookup;
@@ -61,29 +60,11 @@ public class CounterContainer {
 	public void initSideCounters() {
 		isGroupLookup = false;
 		groupFieldName = SearchResult.FAKE_GROUP_FIELD_NAME;
-
-
-		// sideCounters = new long[fieldLookup.size()][][][];
-		// for (int i = 0; i < fieldLookup.size(); i++) {
-		// 	String fieldName = fieldLookup.getKey(i);
-		// 	Lookup side = lookups.get(fieldName);
-		// 	sideCounters[i] = new long[side.size()][1][metricLookup.size()];
-		// }
     }
 
 	public void initGroupedSideCounters(String groupFieldName) {
 		isGroupLookup = true;
 		this.groupFieldName = groupFieldName;
-
-		// sideCounters = new long[fieldLookup.size()][][][];
-		// final Lookup groupSide = lookups.get(groupFieldName);
-		// final int metricLookupSize = metricLookup.size();
-		// final int lookupSize = fieldLookup.size();
-		// for (int f = 0; f < lookupSize; f++) {
-		// 	String fieldName = fieldLookup.getKey(f);
-		// 	Lookup sideLookup = lookups.get(fieldName);
-		// 	sideCounters[f] = new long[sideLookup.size()][groupSide.size()][metricLookupSize];
-		// }
     }
 
     public void add(int fieldId, int columnValueId, int groupFieldValueId, int metricIndex, long metricValue) {
@@ -91,8 +72,6 @@ public class CounterContainer {
 		sideCounters.compute(
 			key, (oldKey, value) -> value == null ? metricValue : metricValue + value
 		);
-		// sideCounters
-			// sideCounters[fieldId][columnValueId][groupFieldValueId][metricIndex] += metricValue;
     }
 
 	public void saveResultTo(Map<GroupedSearchResultRow, Long> groupedResult) {
@@ -118,24 +97,5 @@ public class CounterContainer {
 
 				groupedResult.put(row, value);
 			});
-		// for (int sn = 0; sn < sideCounters.length; sn++) {
-		// 	String sideName = fieldLookup.getKey(sn);
-		// 	final Lookup sideLookup = lookups.get(sideName);
-		// 	for (int sv = 0; sv < sideCounters[sn].length; sv++) {
-		// 		String sideValue = sideLookup.getKey(sv);
-		// 		for (int g = 0; g < sideCounters[sn][sv].length; g++) {
-		// 			String groupFieldValue = isGroupLookup ? groupFieldLookup.getKey(g) : SearchResult.FAKE_GROUP_FIELD_VALUE;
-		// 			for (int m = 0; m < sideCounters[sn][sv][g].length; m++) {
-		// 				String metricName = metricLookup.getKey(m);
-		// 				if(sideCounters[sn][sv][g][m] > 0){
-		// 					GroupedSearchResultRow row = new GroupedSearchResultRow(
-		// 						groupFieldName, groupFieldValue, sideName, sideValue, metricName
-		// 					);
-		// 					groupedResult.put(row, sideCounters[sn][sv][g][m]);
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
 	}
 }
